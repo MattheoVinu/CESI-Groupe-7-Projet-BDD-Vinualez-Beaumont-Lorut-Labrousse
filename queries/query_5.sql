cbb45f3a800bbccf3948d0709132409cb00424fd
MathXb <133019173+MathXb@users.noreply.github.com>
2023-05-12 16:02:07 +0200

    update query_5.sql

--- queries/query_5.sql
+++ queries/query_5.sql
@@ -1,7 +1,8 @@
-select idRegion, sum(donnee) totalEmissions
+select nomRegion, sum(donnee) totalEmissions
 from (select * from gaz where typePollution = 'GES')as  GazGES
 natural join (select * from releve 
 where dateReleve > '2019-12-31'
 and dateReleve < '2021-01-01')as Releves
 join capteur on capteur.idcapteur = Releves.idcapteur
+natural join region
 group by idRegion
